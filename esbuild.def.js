12f9255a861c1cd7bda815c8641d679563a8b29a
Josh Keegan <josh.keegan@gmx.com>
2023-04-19 19:29:09 +0100

    Update yarn build scripts for esbuild & ts

--- esbuild.def.js
+++ esbuild.def.js
@@ -1,6 +1,6 @@
 export default {
     entryPoints: ["src/extension.ts", "src/stylesheet.css"],
-    outdir: "dist",
+    outdir: "build",
     bundle: true,
     // For SpiderMoney updates check GJS release notes: https://gitlab.gnome.org/GNOME/gjs/-/blob/master/NEWS
     // target: "firefox60", // Since GJS 1.53.90
