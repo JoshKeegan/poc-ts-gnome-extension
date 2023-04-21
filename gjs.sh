f2f3b62c7fc6e0e71d50ed1d07b72b1784ff6e52
Josh Keegan <josh.keegan@gmx.com>
2023-04-21 19:19:34 +0100

    Simplify gjs regex esbuild does most of the heavy lifting now so most rules aren't required

--- gjs.sh
+++ gjs.sh
@@ -4,16 +4,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-for src in $(find build -name '*.js'); do
-    sed -i \
-        -e 's/export function/function/g' \
-        -e 's/export var/var/g' \
-        -e 's/export const/var/g' \
-        -e 's/Object.defineProperty(exports, "__esModule", { value: true });/var exports = {};/g' \
-        "$src"
-    sed -i -E \
-        -e "s/import (\w+) from '@girs\/.+'/const { \1 } = imports.gi/g" \
-        -e "s/import \{ (\w+) \} from '.\/(\w+)'/const \1 = Me.imports.\2.\1/g" \
-        -e 's/export class (\w+)/var \1 = class \1/g' \
-        "$src"
-done
+sed -i -E "s/import (\w+) from \"@girs\/.+\"/const { \1 } = imports.gi/g" build/extension.js
